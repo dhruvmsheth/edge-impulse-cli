cbd3aee14c770789fe9f94c1af6cc9f7a01a1f59
Dhruv Sheth <67831664+dhruvsheth-ai@users.noreply.github.com>
2020-11-22 22:12:39 +0530

    Upload this to your Arduino nano 33 ble sense

--- Arduino-nano-33-ble-sense/main.ino
+++ Arduino-nano-33-ble-sense/main.ino
@@ -1,1 +1,31 @@
+#include <Arduino_APDS9960.h>
 
+
+int r, g, b, c, p;
+float sum;
+
+
+void setup()
+{
+    Serial.begin(115200);
+    while(!Serial);
+
+    if(!APDS.begin()) {
+        Serial.println("Could not init APDS9960");
+        while(1);
+    }
+
+}
+
+void loop()
+{
+  if(!APDS.colorAvailable() || !APDS.proximityAvailable()){}
+  else {
+    APDS.readColor(r, g, b, c);
+    sum = r + g + b;
+    p = APDS.readProximity();
+  }
+    float rr = r / sum, gr = g / sum, br = b / sum;
+    Serial.printf("%1.3f,%1.3f,%1.3f\n", rr, gr, br);
+}
+   
